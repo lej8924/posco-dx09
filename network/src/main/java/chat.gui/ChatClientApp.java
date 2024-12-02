9c76ed7ec32d8d627b1a2e98322b242adfb9c280
lej8924 <eunjae8924@naver.com>
2024-12-02 17:07:58 +0900

    🎨 [241202] ChatClientThread와 ChatClientApp 연결 완료

--- network/src/main/java/chat.gui/ChatClientApp.java
+++ network/src/main/java/chat.gui/ChatClientApp.java
@@ -1,30 +1,43 @@
 package chat.gui;
+
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Scanner;
+
+import static chat.ChatServer.PORT;
 
 public class ChatClientApp {
 
 	public static void main(String[] args) {
-		String name = null;
+		String chatName = null;
+		String nickName = null;
 		Scanner scanner = new Scanner(System.in);
 
-		while( true ) {
-			
+
+		while (true) {
 			System.out.println("대화명을 입력하세요.");
 			System.out.print(">>> ");
-			name = scanner.nextLine();
-			
-			if (name.isEmpty() == false ) {
+			chatName = scanner.nextLine();
+
+			System.out.print("닉네임>> ");
+			nickName = scanner.nextLine();
+
+			if (chatName.isEmpty() == false || nickName.isEmpty()==false) {
 				break;
 			}
-			
-			System.out.println("대화명은 한글자 이상 입력해야 합니다.\n");
+
+			System.out.println("대화명은 한글자 이상 입력해야 합니다.");
 		}
-		
+
 		scanner.close();
 
-		new ChatWindow(name).show();
+		new ChatWindow(chatName,nickName).show();
+
+
 	}
-
-
+	public static void log(String message) {
+		System.out.println("[Chat Client] " + message);
+	}
 
 }
